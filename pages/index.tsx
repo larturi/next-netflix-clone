e5a9a5d54b925f5d906cdcf5c3ce49beb43f24f0
Leandro Arturi <lea.arturi@gmail.com>
2023-04-30 13:09:34 -0300

    Auth UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-   return (
-      <>
-         <h1 className='text-2xl text-green-500'>Hola</h1>
-      </>
-   );
+  return (
+    <>
+      <h1 className="text-2xl text-green-500">Hola</h1>
+    </>
+  );
 };
 
 export default Home;
