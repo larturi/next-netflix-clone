daa12a18173bd57956be64afa306f412e2904910
Leandro Arturi <lea.arturi@gmail.com>
2023-05-01 13:07:03 -0300

    Protecting routes logged user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import type { NextPage } from 'next';
+import type { NextPage, NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 const Home: NextPage = () => {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <h1 className="text-2xl text-green-500">Hola</h1>
+      <p>Loged in as: {user?.name}</p>
+      <button onClick={() => signOut()}>SignOut</button>
     </>
   );
 };
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default Home;
