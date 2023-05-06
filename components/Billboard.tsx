af52d489d8b6c67a76306ab275b4ed15d76e9ae0
Leandro Arturi <lea.arturi@gmail.com>
2023-05-06 20:57:39 -0300

    MovieList & MovieCard

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -5,21 +5,54 @@ import useBillboard from '@/hooks/useBillborad';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 const Billboard: React.FC = () => {
-//   const { openModal } = useInfoModalStore();
+  //   const { openModal } = useInfoModalStore();
   const { data } = useBillboard();
 
-//   const handleOpenModal = useCallback(() => {
-//     openModal(data?.id);
-//   }, [openModal, data?.id]);
+  //   const handleOpenModal = useCallback(() => {
+  //     openModal(data?.id);
+  //   }, [openModal, data?.id]);
 
   return (
     <div className="relative h-[56.25vw]">
-      <video poster={data?.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={data?.videoUrl}></video>
+      <video
+        poster={data?.thumbnailUrl}
+        className="
+            w-full 
+            h-[56.25vw] 
+            object-cover 
+            brightness-[60%] 
+            transition 
+            duration-500
+          "
+        autoPlay
+        muted
+        loop
+        src={data?.videoUrl}
+      ></video>
       <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
-        <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+        <p className="
+            text-white 
+            text-1xl 
+            md:text-5xl 
+            h-full 
+            w-[50%] 
+            lg:text-6xl 
+            font-bold 
+            drop-shadow-xl
+          ">
           {data?.title}
         </p>
-        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
+        <p className="
+            text-white 
+            text-[8px] 
+            md:text-lg 
+            mt-3 
+            md:mt-8 
+            w-[90%] 
+            md:w-[80%] 
+            lg:w-[50%] 
+            drop-shadow-xl
+          ">
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
@@ -32,7 +65,8 @@ const Billboard: React.FC = () => {
               bg-opacity-30 
               rounded-md 
               py-1 md:py-2 
-              px-2 md:px-4
+              md:pr-4 md:pl-2
+              pr-3 pl-2
               w-auto 
               text-xs lg:text-lg 
               font-semibold
@@ -42,13 +76,13 @@ const Billboard: React.FC = () => {
               hover:bg-opacity-20
               transition
             "
-            >
-              <AiOutlineInfoCircle className="w-4 md:w-7 mr-1" />
-              More Info
+          >
+            <AiOutlineInfoCircle className="w-4 md:w-7" />
+            More Info
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Billboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import type { NextPage, NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 const Home: NextPage = () => {
+
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      
+      <div className='pb-40'>
+        <MovieList title="Trending now" data={movies} />
+      </div>
     </>
   );
 };
